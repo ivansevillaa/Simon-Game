f7938d235dd85f40d9cc3849155060d4c63393ba
Ivan Sevilla <ivansevillaa@gmail.com>
2019-02-02 11:09:46 -0300

    iluminando secuencia de colores

--- script/script.js
+++ script/script.js
@@ -8,14 +8,55 @@ class Juego {
     constructor() {
         this.inicializar()
         this.generarSecuencia()
+        this.siguienteNivel()
     }
 
     inicializar() {
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
     }
 
     generarSecuencia() {
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+
+    siguienteNivel() {
+        this.iluminarSecuencia()
+    }
+
+    transformarNumeroAColor(numero) {
+        switch (numero) {
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2: 
+                return 'naranja'
+            case 3:
+                return 'verde'
+        }
+    }
+
+    iluminarSecuencia() {
+        for (let i=0; i<this.nivel; i++) {
+            const color = this.transformarNumeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i) 
+        }
+    }
+
+    iluminarColor(color) {
+        this.colores[color].classList.add('light')
+        setTimeout(() => this.apagarColor(color), 350)
+    }
+
+    apagarColor(color) {
+        this.colores[color].classList.remove('light')
     }
 }
 
