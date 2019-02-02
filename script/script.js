cfdc5642b18d81c6791a79841fcfb263ebac2bdc
Ivan Sevilla <ivansevillaa@gmail.com>
2019-02-02 11:25:20 -0300

    Obtaining user input

--- script/script.js
+++ script/script.js
@@ -12,6 +12,7 @@ class Juego {
     }
 
     inicializar() {
+        this.elegirColor = this.elegirColor.bind(this)
         btnEmpezar.classList.add('hide')
         this.nivel = 1
         this.colores = {
@@ -28,6 +29,7 @@ class Juego {
 
     siguienteNivel() {
         this.iluminarSecuencia()
+        this.agregarEventosClick()
     }
 
     transformarNumeroAColor(numero) {
@@ -57,6 +59,17 @@ class Juego {
 
     apagarColor(color) {
         this.colores[color].classList.remove('light')
+    }
+
+    agregarEventosClick() {
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+    }
+
+    elegirColor(event) {
+        console.log(this)
     }
 }
 
